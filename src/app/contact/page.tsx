a8283ae411697e5f01dda6db3394a11f856c819d
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-29 19:34:06 +0600

    Basic routing concept and nested routing

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage(){
+    return (
+        <div>
+            <h1>This is the Contact page</h1>
+        </div>
+    )
+}
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage(){
+    return (
+        <div>
+            <h1>This is the Settings page</h1>
+        </div>
+    )
+}
